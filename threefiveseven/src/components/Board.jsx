87bc380f57e784226abd07ca1954d2ec3f3c6b55
Taro yamashita <taro.c.yamashita@gmail.com>
2018-10-30 13:43:25 -0500

    added piece components

--- threefiveseven/src/components/Board.jsx
+++ threefiveseven/src/components/Board.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import Piece from './Piece.jsx';
 
 class Board extends Component {
+    constructor(props){
+        super(props)
+        this.state = { row1: [0, 0 ,0], row2: [0,0,0,0,0], row3: [0,0,0,0,0,0,0]}
+
+    }
+
     render(){
         return(
             <div>
                 <p>Board Goes Here</p>
+                <div>
+                    {this.state.row1.map((element)=> <Piece />)}
+                </div>
+                <div>
+                    {this.state.row2.map((element)=> <Piece />)}
+                </div>
+                <div>
+                    {this.state.row3.map((element)=> <Piece />)}
+                </div>
+               
             </div>
         )
     }
---
+++ threefiveseven/src/components/Piece.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Piece = (props) =>{
+    return (
+        <button> I'm a Piece button </button>
+    )
+
+}
+
+
+export default Piece; 
